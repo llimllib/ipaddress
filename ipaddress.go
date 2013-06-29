931ed2bf4e04b01eaf995d420f5d6fec25ad789d
Bill Mill <bill.mill@gmail.com>
2013-06-29 00:17:41 -0400

    removing ipv6 function pending more thought

--- ipaddress.go
+++ ipaddress.go
@@ -19,22 +19,6 @@ func IpToUint32(ip net.IP) (uint32, error) {
 	return n, nil
 }
 
-func IpToUint128(ip net.IP) (uint64, uint64, error) {
-	var m, n uint64
-	ip = ip.To16()
-	if ip == nil {
-		return 0, 0, errors.New("not able to convert ip to ipv6")
-	}
-
-	buf := bytes.NewBuffer([]byte(ip[0:7]))
-	binary.Read(buf, binary.BigEndian, &m)
-
-	buf = bytes.NewBuffer([]byte(ip[8:16]))
-	binary.Read(buf, binary.BigEndian, &n)
-
-	return m, n, nil
-}
-
 func IntToIPv4(ip uint32) net.IP {
 	return net.IPv4(byte(ip>>24), byte(ip>>16&0xFF), byte(ip>>8&0xFF), byte(ip&0xFF))
 }
