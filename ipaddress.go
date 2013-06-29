e044af6eb5a0076d435970c6f74e1f6544a97089
Bill Mill <bill.mill@gmail.com>
2013-06-28 23:45:24 -0400

    initial commit

---
+++ ipaddress.go
@@ -0,0 +1,40 @@
+package ipaddress
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"net"
+)
+
+// Assume it's an IPv4
+func IpToUint32(ip net.IP) (uint32, error) {
+	var n uint32
+	ip = ip.To4()
+	if ip == nil {
+		return 0, errors.New("not able to convert ip to ipv4")
+	}
+	buf := bytes.NewBuffer([]byte(ip))
+	binary.Read(buf, binary.BigEndian, &n)
+	return n, nil
+}
+
+func Int128(ip net.IP) (uint64, uint64, error) {
+	var m, n uint64
+	ip = ip.To16()
+	if ip == nil {
+		return 0, 0, errors.New("not able to convert ip to ipv6")
+	}
+
+	buf := bytes.NewBuffer([]byte(ip[0:7]))
+	binary.Read(buf, binary.BigEndian, &m)
+
+	buf = bytes.NewBuffer([]byte(ip[8:16]))
+	binary.Read(buf, binary.BigEndian, &n)
+
+	return m, n, nil
+}
+
+func Int2IPv4(ip int) net.IP {
+	return net.IPv4(byte(ip >> 24), byte(ip >> 16 & 0xFF), byte(ip >> 8 & 0xFF), byte(ip & 0xFF))
+}
---
+++ ipaddress_test.go
@@ -0,0 +1,38 @@
+package ipaddress
+
+import (
+	"log"
+	"net"
+	"testing"
+)
+
+func assertEqual(t *testing.T, expected interface{}, got interface{}) {
+	if got != expected {
+		log.Printf("\nExpected %#v\nbut got  %#v", expected, got)
+		t.FailNow()
+	}
+}
+
+func TestIpToUint32(t *testing.T) {
+	tests := map[string]uint32 {
+		"0.0.0.0": 0,
+		"0.0.0.255": 255,
+		"0.0.1.0": 256,
+		"0.0.1.100": 356,
+		"0.1.0.0": 65536,
+		"0.1.1.1": 65793,
+		"1.0.0.0": 16777216,
+	}
+
+	for test, expected := range tests {
+		i, _ := IpToUint32(net.ParseIP(test))
+		assertEqual(t, i, expected)
+	}
+}
+
+func TestIpToUint32Errors(t *testing.T) {
+	_, err := net.ParseIP("2001:0db8:85a3:0042:1000:8a2e:0370:7334")
+	if !err {
+	  t.FailNow()
+	}
+}
