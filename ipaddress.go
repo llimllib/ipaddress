4f6deea45e27753befdf6f001a6a80ef2ae3f34c
Bill Mill <bill.mill@gmail.com>
2013-06-29 11:24:31 -0400

    add BroadcastAddress function

--- ipaddress.go
+++ ipaddress.go
@@ -7,7 +7,7 @@ import (
 	"net"
 )
 
-// Assume it's an IPv4
+// Convert an ipv4 to a uint32
 func IpToUint32(ip net.IP) (uint32, error) {
 	var n uint32
 	ip = ip.To4()
@@ -19,6 +19,29 @@ func IpToUint32(ip net.IP) (uint32, error) {
 	return n, nil
 }
 
+//convert a uint32 to an ipv4
 func IntToIPv4(ip uint32) net.IP {
 	return net.IPv4(byte(ip>>24), byte(ip>>16&0xFF), byte(ip>>8&0xFF), byte(ip&0xFF))
+}
+
+// Return the broadcast address of a net range. Convert to IPv4 if possible,
+// otherwise return an ipv6
+func BroadcastAddress(n *net.IPNet) net.IP {
+	ip := n.IP.To4()
+	if ip == nil {
+		ip = n.IP
+		return net.IP{
+			ip[0] | ^n.Mask[0], ip[1] | ^n.Mask[1], ip[2] | ^n.Mask[2],
+			ip[3] | ^n.Mask[3], ip[4] | ^n.Mask[4], ip[5] | ^n.Mask[5],
+			ip[6] | ^n.Mask[6], ip[7] | ^n.Mask[7], ip[8] | ^n.Mask[8],
+			ip[9] | ^n.Mask[9], ip[10] | ^n.Mask[10], ip[11] | ^n.Mask[11],
+			ip[12] | ^n.Mask[12], ip[13] | ^n.Mask[13], ip[14] | ^n.Mask[14],
+			ip[15] | ^n.Mask[15]}
+	}
+
+	return net.IPv4(
+		ip[0]|^n.Mask[0],
+		ip[1]|^n.Mask[1],
+		ip[2]|^n.Mask[2],
+		ip[3]|^n.Mask[3])
 }
